518288a8665ad40cbf6361ef685c6c6a70f2e521
Derek Meduri <derekmeduri@gmail.com>
2023-06-09 17:43:49 -0400

    adding ajax functions

--- assets/script.js
+++ assets/script.js
@@ -2,9 +2,15 @@
 var apkiKey = "eebbf925abd804c24b8227298e056052";
 var searchHistory = [];
 
+//functiion to get current weather from user search
 function currentWeather() {
+    //openweather api 
     var weatherUrl = "https://api.openweathermap.org/data/2.5/weather?lat=39.9622601&lon=-83.0007065&units=imperial&appid=eebbf925abd804c24b8227298e056052";
- 
+    //ajax method 
+    $.ajax({
+        url: weatherUrl,
+        method: "GET",
+    })
     //var for city input
     var city = 
 
