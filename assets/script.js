412bac6adbc5290d9a233d1570113e6f37e3c2df
Derek Meduri <derekmeduri@gmail.com>
2023-06-09 17:18:46 -0400

    adding js functions and vars for info

---
+++ assets/script.js
@@ -0,0 +1,20 @@
+//api key
+var apkiKey = "eebbf925abd804c24b8227298e056052";
+var searchHistory = [];
+
+function currentWeather() {
+    var weatherUrl = "https://api.openweathermap.org/data/2.5/weather?lat=39.9622601&lon=-83.0007065&units=imperial&appid=eebbf925abd804c24b8227298e056052";
+ 
+    //var for city input
+    var city = 
+
+    //var for latitude 
+    var lat = 
+    //var for longitude 
+    var lon =
+
+}
+
+function forecastWeather() {
+    var forecastUrl = "https://api.openweathermap.org/data/3.0/onecall?lat=39.9622601&lon=-83.0007065&exclude=current,minutely,hourly,alerts,&units=imperial&appid=eebbf925abd804c24b8227298e056052";
+}
